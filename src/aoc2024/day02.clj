252a69b2081488bfea4c50b8b675e24bf6c5e6dc
Vitaly Samigullin <vrs@pilosus.org>
2024-12-03 10:54:11 +0100

    Day 2, Part 2

--- src/aoc2024/day02.clj
+++ src/aoc2024/day02.clj
@@ -1,37 +1,36 @@
 (ns aoc2024.day02
   (:require
    [aoc2024.tools :as t]
-   [clojure.string :as s]
-   [clojure.string :as str]))
+   [clojure.string :as s]))
 
 (defn safe?
   [vs]
   (loop [items vs
-         idx 0
          prev nil
-         order 0
+         order nil
          result true]
     (if (empty? items)
       result
       (let [curr (first items)]
-        (cond
-          (= idx 0)
-          (recur (rest items) (inc idx) curr order result)
-          (= idx 1)
+        (if (nil? prev)
+          (recur (rest items) curr order result)
           (let [order' (compare curr prev)
-                stop-cond (or (= order' 0)
-                              (> (abs (- curr prev)) 3))
+                stop-cond (or (> (abs (- curr prev)) 3)
+                              (and (not (nil? order)) (not (= order order'))))
                 items' (if stop-cond nil (rest items))
+                prev' curr
                 result' (if stop-cond false result)]
-            (recur items' (inc idx) curr order' result'))
-          :else
-          (let [order' (compare curr prev)
-                stop-cond (or (= order' 0)
-                              (> (abs (- curr prev)) 3)
-                              (not (= order' order)))
-                items' (if stop-cond nil (rest items))
-                result' (if stop-cond false result)]
-            (recur items' (inc idx) curr order' result')))))))
+            (recur items' prev' order' result')))))))
+
+(defn safe-tolerated?
+  [vs]
+  (loop [head-items vs
+         tail-items (list)]
+    (if (safe? (into head-items (rest tail-items)))
+      true
+      (if (empty? head-items)
+        false
+        (recur (rest head-items) (conj tail-items (first head-items)))))))
 
 (defn part1
   "Part 1: (part1)
@@ -44,5 +43,19 @@
     (reduce
      (fn [acc v]
        (if (safe? v) (inc acc) acc))
+     0
+     data)))
+
+(defn part2
+  "Part 2: (part2)
+  => 585, 600 - too low
+  Test: (part2 :test? true)"
+  [& {:keys [test?] :or {test? false}}]
+  (let [lines (-> (t/input-path test?)
+                  t/path->lines)
+        data (map t/str->ints lines)]
+    (reduce
+     (fn [acc v]
+       (if (safe-tolerated? v) (inc acc) acc))
      0
      data)))
