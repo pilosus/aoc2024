23d2a8a668c4643cf4a4e215d89ebf4a74892888
Vitaly Samigullin <vrs@pilosus.org>
2024-12-03 19:20:03 +0100

    Day 3, Part 2

--- src/aoc2024/day03.clj
+++ src/aoc2024/day03.clj
@@ -5,9 +5,11 @@
 
 (def mul-regexp #"mul\((?<left>\d+)\,(?<right>\d+)\)")
 
+(def mul-do-regexp #"mul\((?<left>\d+)\,(?<right>\d+)\)|do\(\)|don't\(\)")
+
 (defn part1
   "Part 1: (part1)
-  =>
+  => 164730528
   Test: (part1 :test? true)"
   [& {:keys [test?] :or {test? false}}]
   (let [lines (-> (t/input-path test?)
@@ -19,3 +21,34 @@
          (+ acc result)))
      0
      (re-seq mul-regexp line))))
+
+(defn proc
+  [s]
+  (loop [items (re-seq mul-do-regexp s)
+         skip? false
+         acc 0]
+    (if (empty? items)
+      acc
+      (let [[op l r] (first items)
+            skip' (cond
+                    (s/starts-with? op "don't(") true
+                    (s/starts-with? op "do(") false
+                    :else skip?)
+            acc' (if (and (s/starts-with? op "mul(") (not skip?))
+                   (+ acc (* (t/str->int l) (t/str->int r)))
+                   acc)]
+
+        (recur
+         (rest items)
+         skip'
+         acc')))))
+
+(defn part2
+  "Part 2: (part2)
+  => 70478672
+  Test: (part2 :test? true)"
+  [& {:keys [test?] :or {test? false}}]
+  (let [lines (-> (t/input-path test?)
+                  t/path->lines)
+        line (s/join lines)]
+    (proc line)))
