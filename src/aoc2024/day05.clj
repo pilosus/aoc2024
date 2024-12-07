aa4caf1dc2c3d1ae80f54f989a5e3a8cc47561fe
Vitaly Samigullin <vrs@pilosus.org>
2024-12-07 17:22:21 +0100

    Day 5, Part 2

--- src/aoc2024/day05.clj
+++ src/aoc2024/day05.clj
@@ -3,12 +3,16 @@
    [aoc2024.tools :as t]
    [clojure.string :as s]))
 
+(defn overlap
+  [actual excluded-by-rule]
+  (clojure.set/intersection
+   (set actual)
+   (set excluded-by-rule)))
+
 (defn not-contains?
   [actual excluded-by-rule]
   (empty?
-   (clojure.set/intersection
-    (set actual)
-    (set excluded-by-rule))))
+   (overlap actual excluded-by-rule)))
 
 (defn check-item
   [item rules]
@@ -30,6 +34,56 @@
   (reduce
    (fn [acc item]
      (+ acc (check-item item rules)))
+   0
+   data))
+
+(defn b->a
+  [before after overlaping]
+  (let [before' (into [] (remove overlaping before))
+        o' (into [] overlaping)
+        after' (into o' after)]
+    [before' after']))
+
+(defn a->b
+  [before after overlaping]
+  (let [after' (into [] (remove overlaping after))
+        o' (into [] overlaping)
+        before' (into before o')]
+    [before' after']))
+
+(defn order-iteration
+  [n before after rules]
+  (let [{:keys [a b]} (get rules n)
+        ba (overlap before a)
+        ab (overlap after b)]
+    (cond
+      (seq ba) (b->a before after ba)
+      (seq ab) (a->b before after ab)
+      :else [before after])))
+
+(defn correct-item
+  [item rules]
+  (loop [before []
+         after item]
+    (let [check-result (check-item (into before after) rules)]
+      (if (not= 0 check-result)
+        check-result
+        (let [n (first after)
+              [before' after'] (order-iteration n before (rest after) rules)]
+          (if (empty? after)
+            ;; start from the beginning
+            (recur [] before)
+            ;; adjusted before/after
+            (recur (conj before' n) after')))))))
+
+(defn calc-corrected
+  [data rules]
+  (reduce
+   (fn [acc item]
+     (let [r (check-item item rules)]
+       (if (= r 0)
+         (+ acc (correct-item item rules))
+         acc)))
    0
    data))
 
@@ -61,3 +115,11 @@
   [& {:keys [test?] :or {test? false}}]
   (let [[rules data] (get-input test?)]
     (check-order data rules)))
+
+(defn part2
+  "Part 2: (part2)
+  => 5564
+  Test: (part2 :test? true)"
+  [& {:keys [test?] :or {test? false}}]
+  (let [[rules data] (get-input test?)]
+    (calc-corrected data rules)))
